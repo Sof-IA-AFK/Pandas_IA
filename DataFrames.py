dbcd6962c92aac76d75805e9d5f54e5f4e8b3af5
Sof-IA-AFK <sofiprins6@gmail.com>
2024-10-05 09:59:59 -0500

    Abordamos indexación avanzada en Pandas

--- DataFrames.py
+++ DataFrames.py
@@ -1,8 +1,22 @@
 import pandas as pd
+datos = {'Nombre': ['Alice', 'Bob', 'Charlie'],
+         'Edad': [25, 30, 35],
+         'Ciudad': ['A', 'B', 'C']}
 
-datos = {'Nombre':['Hermione', 'Harry', 'Ron'],
-          'Edad': [14, 13, 14],
-          'Ciudad': ['Inglaterra','Estados Unidos', 'Londres']}
+df = pd.DataFrame(datos, index=['a', 'b', 'c'])
 
-df = pd.DataFrame(datos)
 print(df)
+
+#Seleccionar datos con loc[] # multiples valores    
+seleccion_loc = df.loc['b', 'Edad'] 
+print(seleccion_loc)
+#Seleccionar datos con iloc[]
+seleccionar_iloc = df.iloc[1, 1]
+print(seleccionar_iloc)
+#Seleccionar datos con at[] #si solo necesitaramos un valor
+seleccion_at = df.at['b', 'Edad']
+print(seleccion_at)
+#Seleccionar datos con iat[]
+seleccion_iat = df.iat[1, 1]
+print(seleccion_iat)
+
