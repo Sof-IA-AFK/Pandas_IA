ca9158505264eed4a718a47368ccf12be12c1a59
Sof-IA-AFK <sofiprins6@gmail.com>
2024-10-05 10:05:50 -0500

    Abordamos formas de construir DataFrames

--- formas_de_construir_un_DataFrame.py
+++ formas_de_construir_un_DataFrame.py
@@ -3,5 +3,18 @@ import pandas as pd
 data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada', 'Nevada'],
         'year': [2000, 2001, 2002, 2001, 2002, 2003],
         'pop': [1.5, 1.7, 3.6, 2.4, 2.9, 3.2]}
+
 frame = pd.DataFrame(data)
+
 print(frame)
+
+frame_reordenado = pd.DataFrame(data, columns=['year', 'state', 'pop'])
+print(frame_reordenado)
+
+frame2 = pd.DataFrame(data, columns=['year', 'state', 'pop', 'debt'],
+                      index=['one', 'two', 'three', 'four', 'five', 'six'])
+
+print(frame2)
+print(frame2.columns)
+print(frame2['state'])
+print(frame2.year)
